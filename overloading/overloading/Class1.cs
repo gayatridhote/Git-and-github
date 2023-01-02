f9874d73b76bfd5b5a9a667701ee118d165e9e2d
Gayatri Dhote <gayatridhote996@gmail.com>
2023-01-02 17:29:59 +0530

    add file from vs

---
+++ overloading/overloading/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace overloading
+{
+    class Addition
+    {
+        public void Add(int a)
+        {
+            Console.WriteLine("Addition is " + a);
+        }
+        public void Add(int a,int b)
+        {
+            int c = a + b;
+            Console.WriteLine("Addition is " + c);
+        }
+        public void Add(int a,double c)
+        {
+            double b = (double)(a + c);
+            Console.WriteLine("Addition is " + b);
+        }
+        public void Add(int a , string m)
+        {
+            Console.WriteLine("Addition is " + a+m);
+        }
+        public void Add(int a,double b,string c)
+        {
+            double d = (double)a + b;
+            Console.WriteLine("Addition is " + d +c);
+        }
+    }
+}
---
+++ overloading/overloading/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition num = new Addition();
+            num.Add(20);
+            num.Add(10,20);
+            num.Add(10, 45.32);
+            num.Add(4, " students");
+            num.Add(10, 6.4, "degree");
+
+            Console.ReadKey();
+        }
+    }
+}
